4655ce30892d47eefd413b64dd0acfe24e805a2b
mur1ll0 <murillomaleski@gmail.com>
2019-03-22 16:13:23 -0300

    Ajustes do bootstrap
    Ajustes do bootstrap

--- buscador.php
+++ buscador.php
@@ -1,44 +1,54 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 
 <?php
 	require 'comum.php';
-	//require 'static\stylesheets\buscador.css';
 	
 	session_start();
 	
-	c_header();
+	c_header_sideless();
 ?>
 
 
-<div class="aw-layout-page">	
+<div class="aw-layout-page">
 	<section>
-		<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img src="https://upload.wikimedia.org/wikipedia/commons/8/8d/Yarra_Night_Panorama%2C_Melbourne_-_Feb_2005.jpg" class="d-block w-100" alt="...">
-				</div>
-				<div class="carousel-item">
-					<img src="https://upload.wikimedia.org/wikipedia/commons/8/8d/Yarra_Night_Panorama%2C_Melbourne_-_Feb_2005.jpg" class="d-block w-100" alt="...">
-				</div>
-				<div class="carousel-item">
-					<img src="https://upload.wikimedia.org/wikipedia/commons/8/8d/Yarra_Night_Panorama%2C_Melbourne_-_Feb_2005.jpg" class="d-block w-100" alt="...">
-				</div>
-				<div class="carousel-item">
-					<img src="https://upload.wikimedia.org/wikipedia/commons/8/8d/Yarra_Night_Panorama%2C_Melbourne_-_Feb_2005.jpg" class="d-block w-100" alt="...">
-				</div>
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  </ol>
+		 
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner">
+			<div class="item active">
+			  <img src="http://placehold.it/1200x315" alt="...">
+			  <div class="carousel-caption">
+				<h3>Caption Text</h3>
+			  </div>
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
+			<div class="item">
+			  <img src="http://placehold.it/1200x315" alt="...">
+			  <div class="carousel-caption">
+				<h3>Caption Text</h3>
+			  </div>
+			</div>
+			<div class="item">
+			  <img src="http://placehold.it/1200x315" alt="...">
+			  <div class="carousel-caption">
+				<h3>Caption Text</h3>
+			  </div>
+			</div>
+		  </div>
+		 
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		  </a>
+		</div> <!-- Carousel -->
 	</section>
 	<section class="search-sec">
 		<div class="container">
@@ -72,10 +82,9 @@
 			</form>
 		</div>
 	</section>
-
-
 </div>
+
 	
 <?php
-	//c_footer();
+	c_footer_sideless();
 ?>
--- comum.php
+++ comum.php
@@ -3,7 +3,7 @@
 #          --RECURSOS EM COMUM ENTRE AS PÁGINAS--         #
 #                                                         #
 #                Autor: Murillo André Maleski             #
-#                   Diário de Bordo Online                #
+#                          SERVLOC                        #
 #                                                         #
 #  Funções para adicionar recursos básicos às páginas do  #
 #  projeto, evitando repetição de código desnecessária.   #
@@ -125,6 +125,65 @@
 <?php
 	}
 	
+	function c_header_sideless(){
+?>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Servloc</title>
+		<link rel="stylesheet" type="text/css" href="static/layout/stylesheets/vendors.min.css"/>
+		<link rel="stylesheet" type="text/css" href="static/layout/stylesheets/algaworks.min.css"/>
+		<link rel="stylesheet" type="text/css" href="static/layout/stylesheets/application.css"/>
+		<link rel="stylesheet" type="text/css" href="static/stylesheets/trab.css">
+		<link rel="stylesheet" type="text/css" href="static/stylesheets/multi-select.css"/>
+		<link rel="stylesheet" type="text/css" href="static/stylesheets/amsify.suggestags.css"/>
+		<script type="text/javascript" src="static/sprite/iconwc.js"></script>
+	</head>
+	
+	<body>
+		<nav class="navbar  navbar-fixed-top  navbar-default  js-sticky-reference" id="main-navbar">
+		  <div class="container-fluid">
+	
+		  	<div class="navbar-header">
+		      <a class="navbar-brand  hidden-xs" href="#">
+		        <img alt="SERVLOC" src="static/img/logonada.png"/>
+		      </a>
+		    </div>
+	
+		    <ul class="nav navbar-nav  navbar-right">
+		      
+		      <li class="dropdown">
+		        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+		          <i class="fa  fa-user"></i>
+		        </a>
+		        
+		        <ul class="dropdown-menu">
+		          <li>
+		            <div class="aw-logged-user">
+		              <img src="https://api.adorable.io/avatars/80/joaodascouves" 
+		                width="80" height="80" alt="João das Couves" class="aw-logged-user__picture" />
+		              <span class="aw-logged-user__name">João das Couves</span>
+		            </div>
+		          </li>
+		          <li role="separator" class="divider"></li>
+		          <li><a href="#">Meu perfil</a></li>
+		          <li><a href="#">Alterar senha</a></li>
+		          <li><a href="#">Pagamentos</a></li>
+		        </ul>
+		      </li>
+		      
+		      <li>
+		        <a href="#"><em class="fa  fa-sign-out"></em></a>
+		      </li> 
+		    </ul>
+	
+			</div>
+		</nav>
+	
+<?php
+	}
+	
+	
 	/*---------------------------------
 	--        Rodapé da Página       --
 	---------------------------------*/
@@ -141,7 +200,7 @@
 		
 		<script src="static/layout/javascripts/vendors.min.js"></script>
 		<script src="static/layout/javascripts/algaworks.min.js"></script>
-		<script src="static/javascripts/vendors/jquery.maskMoneymin.js"></script>
+		<script src="static/javascripts/vendors/jquery.maskMoney.min.js"></script>
 		<script src="static/javascripts/trab.js"></script>
 		<script src="static/javascripts/vendors/jquery.multi-select.js" type="text/javascript"></script>
 		<script src="static/javascripts/vendors/jquery.amsify.suggestags.js" type="text/javascript"></script>
@@ -167,6 +226,48 @@
 
 <?php
 	}
+	
+	function c_footer_sideless(){
+?>
+		<div>
+			<footer class="aw-layout-footer  js-content">
+				<div class="container-fluid">
+					<span class="aw-footer-disclaimer">Universidade Federal da Fronteira Sul - UFFS</span>
+				</div>
+			</footer>
+		</div>
+	</div>
+		
+		<script src="static/layout/javascripts/vendors.min.js"></script>
+		<script src="static/layout/javascripts/algaworks.min.js"></script>
+		<script src="static/javascripts/vendors/jquery.maskMoney.min.js"></script>
+		<script src="static/javascripts/trab.js"></script>
+		<script src="static/javascripts/vendors/jquery.multi-select.js" type="text/javascript"></script>
+		<script src="static/javascripts/vendors/jquery.amsify.suggestags.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			//OPTGROUP
+			$('#optgroup').multiSelect({ selectableOptgroup: true });
+			
+			//SUGGESTAGS
+			$('input[name="tags"]').amsifySuggestags({
+			  type : 'bootstrap',
+			  suggestions: ['Black', 'White', 'Red', 'Blue', 'Green', 'Orange'],
+			  afterAdd: function(value) {
+				// after add
+			  },
+			  afterRemove: function(value) {
+				// after remove
+			  }
+			});
+			$('.js-sidebar, .js-content').toggleClass('is-toggled');
+		</script>
+		
+		</body>
+	</html>
+
+<?php
+	}
+	
 	
 	/*---------------------------------
 	--             --
