58421364e4b11880d0648cd1352334d41790f838
rodolfotr <rodolfotrevisol015@gmail.com>
2019-03-28 21:56:27 -0300

    001- Criação banco e tabela servicos
    Adicionado campo "ativo" para usar na exclusão de serviços fornecidos.
    Alterado também o nome da tabela de "Servico" para "Servicos"

--- db/migration/001-servlocbd.sql
+++ db/migration/001-servlocbd.sql
@@ -4,10 +4,11 @@
 CREATE DATABASE IF NOT EXISTS `servlocbd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `servlocbd`;
 
-CREATE TABLE servico(
+CREATE TABLE servicos(
 	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	titulo varchar(64) NOT NULL,
 	descricao varchar(200),
 	estado varchar (20),
-	cidade varchar (40)
+	cidade varchar (40),
+	ativo integer (1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
