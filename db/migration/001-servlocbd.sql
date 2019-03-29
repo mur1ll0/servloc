5c503f457017fcbe03f8100fdc11e069eaec0c0a
mur1ll0 <murillomaleski@gmail.com>
2019-03-28 21:23:28 -0300

    BD001 Tabelas Serviço
    Para cadastrar os serviços
    001

--- db/migration/001-servlocbd.sql
+++ db/migration/001-servlocbd.sql
@@ -1,1 +1,13 @@
-CREATE DATABASE servlocbd;
+--
+-- Database: `servlocbd`
+--
+CREATE DATABASE IF NOT EXISTS `servlocbd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `servlocbd`;
+
+CREATE TABLE servico(
+	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	titulo varchar(64) NOT NULL,
+	descricao varchar(200),
+	estado varchar (20),
+	cidade varchar (40)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
