cc41d78af057c3c11e3907a3a241a8a32fe70f2f
Murillo Andre Maleski <murillomaleski@gmail.com>
2019-05-22 20:53:41 -0300

    Alterado connection para 'localhost'

--- static/php/connection.php
+++ static/php/connection.php
@@ -15,7 +15,7 @@
 	---------------------------------*/
 	function connect_db(){
 
-		$db_host = '127.0.0.1';
+		$db_host = 'localhost';
 		$db_port = '3306';
 		$db_database = 'servlocbd';
 		$db_user = 'root';
